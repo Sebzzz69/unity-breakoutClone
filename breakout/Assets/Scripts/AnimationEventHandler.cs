b9d475a5f0be81de220aec69a8b39a2839453942
Okiman42 <antonaxel05@gmail.com>
2024-05-15 10:57:12 +0200

    spawns powerup

--- breakout/Assets/Scripts/AnimationEventHandler.cs
+++ breakout/Assets/Scripts/AnimationEventHandler.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class AnimationEventHandler : MonoBehaviour
 {
+    public GameObject powerUpPrefab; // Reference to the power-up prefab to spawn
+    public int randomInt;
+
+    void GenerateRandomNumber()
+    {
+        // Generate a random number between 1 and 10 (inclusive)
+        randomInt = Random.Range(1, 11);
+    }
+
+
+    public void SpawnPowerUp()
+    {
+
+        // Check if powerUpPrefab is assigned
+        if (powerUpPrefab != null)
+        {
+            // Spawn the power-up at the current position
+            Instantiate(powerUpPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
     public void DestroyAfterAnimation()
     {
-        Destroy(this.gameObject);
+       GenerateRandomNumber();
+
+        if (randomInt == 1)
+            SpawnPowerUp();
+        Debug.Log(randomInt);
+
+        Destroy(gameObject);
     }
+
+   
 }
