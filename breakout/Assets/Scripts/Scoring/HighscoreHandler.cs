facb91d247c8b7257bec50f52bdff675c9a86911
Sebzzz69 <callenholmljungsebastian@gmail.com>
2024-05-17 18:05:37 +0200

    save score with JSON

--- breakout/Assets/Scripts/Scoring/HighscoreHandler.cs
+++ breakout/Assets/Scripts/Scoring/HighscoreHandler.cs
@@ -6,6 +6,8 @@ public class HighscoreHandler : MonoBehaviour
 {
     List<HighscoreElement> highscoreList = new List<HighscoreElement>();
     [SerializeField] int maxCount = 7;
+    [SerializeField] string filename;
+
 
     private void Start()
     {
@@ -14,17 +16,37 @@ public class HighscoreHandler : MonoBehaviour
 
     private void LoadHighScore()
     {
+        highscoreList = FileHandler.ReadFromJSON<HighscoreElement>(filename);
 
+        while (highscoreList.Count > maxCount)
+        {
+            highscoreList.RemoveAt(maxCount);
+        }
     }
 
     private void SaveHighScore()
     {
-
+        FileHandler.SaveToJSON<HighscoreElement>(highscoreList, filename);
     }
 
     public void AddHighScoreIfPossible(HighscoreElement element)
     {
+        for (int i = 0; i < maxCount; i++)
+        {
+            if (i >= highscoreList.Count || element.points > highscoreList[i].points)
+            {
+                // Add new high score
+                highscoreList.Insert(i, element);
 
+                while (highscoreList.Count > maxCount)
+                {
+                    highscoreList.RemoveAt(maxCount);
+                }
+
+                SaveHighScore();
+                break;
+            }
+        }
     }
 
 }
